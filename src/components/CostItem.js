5c758e0b9bbed7e6808d5b23d6fb2c9588122b3d
Y@n <Zinchenko.yan@gmail.com>
2022-10-29 09:45:15 +0300

    creat component

--- src/components/CostItem.js
+++ src/components/CostItem.js
@@ -1,5 +1,15 @@
+import './CostItem.css';
+
 function CostItem() {
-    return <h2>Element </h2>;
+    return (
+        <div className='cost-item'>
+            <div>март 2021 12</div>
+            <div className='cost-item__description'>
+                <h2>Холодильник</h2>
+                <div className='cost-item__price'>$999.99</div>
+            </div>
+        </div>
+    );
 }
 
 export default CostItem;
